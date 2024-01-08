a74b587508738b49d4b414695a0b67a1796dab45
Veniamin <47269098+veniaminvelkov@users.noreply.github.com>
2024-01-08 23:48:01 +0200

    Implemented a new frame for adding tasks into the database.

---
+++ TaskManager/src/AddFrame.java
@@ -0,0 +1,102 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class AddFrame extends JFrame{
+	private JTextField titleField, descriptionField, deadlineField, priorityField;
+    private JButton addButton;
+    private JLabel messageLabel;
+    
+    Connection conn = null;
+
+    public AddFrame() {
+        setTitle("Add a task");
+        setSize(300, 350);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+        
+        JPanel upPanel = new JPanel();
+        upPanel.setLayout(new GridLayout(4, 2));
+        
+        JPanel downPanel = new JPanel();
+        downPanel.setLayout(new GridLayout(2,1));
+
+        
+        upPanel.add(new JLabel("Title:"));
+        titleField = new JTextField();
+        upPanel.add(titleField);
+
+        upPanel.add(new JLabel("Description:"));
+        descriptionField = new JTextField();
+        upPanel.add(descriptionField);
+
+        upPanel.add(new JLabel("Deadline:"));
+        deadlineField = new JTextField();
+        upPanel.add(deadlineField);
+
+        upPanel.add(new JLabel("Priority:"));
+        priorityField = new JTextField();
+        upPanel.add(priorityField);
+        
+        int padding = 10;
+
+        upPanel.setBorder(BorderFactory.createEmptyBorder(padding, padding, padding, padding));
+
+        addButton = new JButton("Add");
+		
+        addButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String title = titleField.getText();
+                String description = descriptionField.getText();
+                String deadline = deadlineField.getText();
+                String priority = priorityField.getText();
+
+                if (title.isEmpty() || description.isEmpty() || deadline.isEmpty() || priority.isEmpty()) {
+                    messageLabel.setText("None of the fields can be empty.");
+                }
+                else {
+                    try {
+                    	conn=DBConnection.getConnection();
+            			String sql = "INSERT INTO \"Task\" (title, description, deadline, priority) VALUES (?, ?, ?, ?)";
+
+                        PreparedStatement statement = conn.prepareStatement(sql);
+                        statement.setString(1, title);
+                        statement.setString(2, description);
+                        statement.setString(3, deadline);
+                        statement.setString(4, priority);
+
+                        int rowsInserted = statement.executeUpdate();
+                        if (rowsInserted > 0) {
+                            dispose();
+                        }
+
+                        statement.close();
+                        conn.close();
+                    } catch (SQLException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        messageLabel = new JLabel();
+        messageLabel.setForeground(Color.RED);
+        downPanel.add(messageLabel);
+        downPanel.add(addButton);
+        
+        this.add(upPanel, BorderLayout.PAGE_START);
+		this.add(downPanel, BorderLayout.PAGE_END);
+    }
+}
